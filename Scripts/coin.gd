f8072a1b1c3b6c60c1eff8f9fe4c496a2fa2488a
javier1115 <jruizcastro@ftstudent.org>
2026-03-11 10:27:23 -0400

    wsvfv

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 @onready var game_manager = %"Game Manager"
-
+@onready var animation = $AnimationPlayer
 func _on_body_entered(body):
 	game_manager.add_point() 
-	queue_free()
+	animation.play("pickup")
 	
